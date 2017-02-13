7d78a4c6a2e3dd3ca2d259a8624cc893df95da62
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-13 20:38:33 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,11 +6,15 @@ class GroupsController < ApplicationController
   def show
  @group = Group.find(params[:id])
  end
+ 
+ def edit
+ @group = Group.find(params[:id])
+end
 
   def new
    @group = Group.new
   end
-  
+
   def create
  @group = Group.new(group_params)
  @group.save
