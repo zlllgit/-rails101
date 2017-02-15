1263175b1a9eba970df56045f6c017027a37a6fc
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-15 20:17:07 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
-      before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]  def index
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+          def index
     @groups = Group.all
   end
 
@@ -8,11 +10,6 @@ class GroupsController < ApplicationController
  end
 
  def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def new
@@ -30,12 +27,6 @@ class GroupsController < ApplicationController
     end
 
     def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -44,20 +35,23 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-     @group = Group.find(params[:id])
-
-     if current_user != @group.user
-       redirect_to root_path, alert: "You have no permission."
-     end
-
-     @group.destroy
-     redirect_to groups_path, alert: "Group deleted"
-   end
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
 
 
  private
 
-def group_params
-  params.require(:group).permit(:title, :description)
- end
+ def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
