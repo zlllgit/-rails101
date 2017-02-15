68bd41cb3c66fb1b7c0408629ca065e96ad3c921
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-15 20:43:36 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,9 @@ class GroupsController < ApplicationController
   end
 
   def show
- @group = Group.find(params[:id])
- end
+    @group = Group.find(params[:id])
+    @posts = @group.posts
+  end
 
  def edit
   end
