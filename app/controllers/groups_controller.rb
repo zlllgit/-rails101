35c1813a0c31f63195ec71a141b6ed5ef949dbe9
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-15 20:52:22 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,9 @@ class GroupsController < ApplicationController
   end
 
   def show
-      @group = Group.find(params[:id])
-    @posts = @group.posts.recent
-    end
+   @group = Group.find(params[:id])
+   @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+ end
 
  def edit
   end
