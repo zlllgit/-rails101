e0d70c117499ed6224271545810be746146680b7
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-15 20:45:02 +0800

    add order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,9 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
-    @posts = @group.posts
-  end
+      @group = Group.find(params[:id])
+      @posts = @group.posts.order("created_at DESC")
+    end
 
  def edit
   end
